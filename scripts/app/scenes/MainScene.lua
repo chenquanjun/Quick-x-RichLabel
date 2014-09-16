007e1ab193bdf1ff9950a92c792a61bd74211cd9
chenquanjun <chenshiyouxiang@gmail.com>
2014-09-16 11:56:08 +0800

    format

--- scripts/app/scenes/MainScene.lua
+++ scripts/app/scenes/MainScene.lua
@@ -17,6 +17,21 @@ function MainScene:ctor()
 					}
 	local testLabel = RichLabel:create(params)
 	self:addChild(testLabel)
+
+	testLabel:setPosition(display.cx, display.cy)
+
+	self:performWithDelay(function () --大小测试
+		testLabel:setDimensions(CCSize(300, 200))
+		local size = testLabel:getLabelSize()
+		print("CurSize:"..size.width.." "..size.height)
+	end, 2)
+
+	self:performWithDelay(function () --重新设置文字测试
+		testLabel:setLabelString("[fontColor=f75d85 fontSize=20]hello world[/fontColor][fontColor=fefefe]这是测试代码[/fontColor]")
+	
+		local size = testLabel:getLabelSize()
+		print("CurSize:"..size.width.." "..size.height)
+	end, 3)
 end
 
 function MainScene:onEnter()
--- scripts/app/scenes/RichLabel.lua
+++ scripts/app/scenes/RichLabel.lua
@@ -8,13 +8,18 @@ RichLabel.__index      = RichLabel
 RichLabel._fontName = nil
 RichLabel._fontSize = nil
 RichLabel._fontColor = nil
+RichLabel._containLayer = nil --装载layer
+RichLabel._spriteArray = nil --精灵数组
+RichLabel._textStr = nil
+RichLabel._maxWidth = nil
+RichLabel._maxHeight = nil
 
 --目前支持参数
 --[[
 	文字 
 	fontName  : font name
 	fontSize  : number
-	fontColor : ccc3(r,g,b) 字符串用十六进制，设置用ccc3
+	fontColor : ccc3(r,g,b) 字符串用十六进制，通用设置用ccc3
 
 	图片
 	image : "xxx.png"
@@ -29,49 +34,71 @@ end
 
 function RichLabel:init(params)
 	local text = params.text
-	local fontName = params.fontName or "Arial"
-	local fontSize = params.fontSize or 30
+
+	--如果text的格式指定字体则使用指定字体，否则使用默认字体
+	--大小和颜色同理
+	local fontName = params.fontName or "Arial" --默认字体
+	local fontSize = params.fontSize or 30 --默认大小
 	local fontColor = params.fontColor or ccc3(255, 255, 255)
 	local dimensions = params.dimensions or CCSize(0, 0) --默认无限扩展
 
-	-- do --test
-	-- 	local bgSprite = CCSprite:createWithTexture(nil, CCRect(0, 0, dimensions.width, dimensions.height))
-	-- 	bgSprite:setAnchorPoint(ccp(0, 1))
-	-- 	bgSprite:setColor(ccc3(255, 0, 0))
-	-- 	self:addChild(bgSprite)
-	-- end
+	self._dimensions = dimensions
+
+	--装文字和图片精灵
+	local containLayer = display.newLayer()
+	self:addChild(containLayer)
+	self._containLayer = containLayer
 
     self._fontName = fontName
     self._fontSize = fontSize
     self._fontColor = fontColor
-
-	--转化好的数组
-	local parseArray = self:parseString(text)
-
-	--将字符串拆分成一个个字符
-	self:formatString(parseArray)
-
-	-- dump(parseArray)
-
-	--创建精灵
-	local spriteArray = self:createSprite(parseArray)
-
-	--调整精灵位置
-	self:adjustPosition(spriteArray, dimensions)
-
-	--test
-	self:setPosition(display.cx - 200, display.cy)
-
+   
+    self:setLabelString(text)
 end
 
-function RichLabel:adjustPosition(spriteArray, dimensions)
-	local totalWidth = dimensions.width
-	local totalHight = dimensions.height
+--设置text
+function RichLabel:setLabelString(text)
+	if self._textStr == text then
+		return --相同则忽略
+	end
 
+	if self._textStr then --删除之前的string
+		self._spriteArray = nil
+		self._containLayer:removeAllChildren()
+	end
+
+	self._textStr = text
+	
+	--转化好的数组
+	local parseArray = self:parseString_(text)
+
+	--将字符串拆分成一个个字符
+	self:formatString_(parseArray)
+
+	--创建精灵
+	local spriteArray = self:createSprite_(parseArray)
+	self._spriteArray = spriteArray
+
+	self:adjustPosition_()
+end
+
+--设置大小
+function RichLabel:setDimensions(dimensions)
+	self._containLayer:setContentSize(dimensions)
+	self._dimensions = dimensions
+
+	self:adjustPosition_()
+end
+
+function RichLabel:getLabelSize()
+	local width = self._maxWidth or 0
+	local height = self._maxHeight or 0
+	return CCSize(width, height)
+end
+
+function RichLabel:getSizeOfSprites_(spriteArray)
 	local widthArr = {} --宽度数组
 	local heightArr = {} --高度数组
-
-	local spriteNum = #spriteArray
 
 	--精灵的大小
 	for i, sprite in ipairs(spriteArray) do
@@ -79,16 +106,29 @@ function RichLabel:adjustPosition(spriteArray, dimensions)
 		widthArr[i] = contentSize.width
 		heightArr[i] = contentSize.height
 	end
+	return widthArr, heightArr
+
+end
+
+function RichLabel:getPointOfSprite_(widthArr, heightArr, dimensions)
+	local totalWidth = dimensions.width
+	local totalHight = dimensions.height
+
+	local maxWidth = 0
+	local maxHeight = 0
+
+	local spriteNum = #widthArr
 
 	--从左往右，从上往下拓展
-	local curX = 0
+	local curX = 0 --当前x坐标偏移
 	
-	local curIndexX = 1
-	local curIndexY = 1
+	local curIndexX = 1 --当前横轴index
+	local curIndexY = 1 --当前纵轴index
 	
 	local pointArrX = {} --每个精灵的x坐标
+
 	local rowIndexArr = {} --行数组，以行为index储存精灵组
-	local indexArrY = {}
+	local indexArrY = {} --每个精灵的行index
 
 	--计算宽度，并自动换行
 	for i, spriteWidth in ipairs(widthArr) do
@@ -108,8 +148,8 @@ function RichLabel:adjustPosition(spriteArray, dimensions)
 			curIndexX = 1 --x坐标重置
 			curIndexY = curIndexY + 1 --y坐标自增
 		else
-			pointX = curX + halfWidth
-			curX = pointX + halfWidth
+			pointX = curX + halfWidth --精灵坐标x
+			curX = pointX + halfWidth --精灵最右侧坐标
 			curIndexX = curIndexX + 1
 		end
 		pointArrX[i] = pointX --保存每个精灵的x坐标
@@ -122,41 +162,69 @@ function RichLabel:adjustPosition(spriteArray, dimensions)
 			tmpIndexArr = {}
 			rowIndexArr[rowIndex] = tmpIndexArr
 		end
-		tmpIndexArr[#tmpIndexArr + 1] = i
+		tmpIndexArr[#tmpIndexArr + 1] = i --保存相同行对应的精灵
+
+		if curX > maxWidth then
+			maxWidth = curX
+		end
 	end
 
-	-- dump(rowIndexArr)
-
 	local curY = 0
-	local pointArrY = {} --每一行的y坐标
+	local rowHeightArr = {} --每一行的y坐标
 
 	--计算每一行的高度
 	for i, rowInfo in ipairs(rowIndexArr) do
-		local maxHeight = 0
-		for j, index in ipairs(rowInfo) do
+		local rowHeight = 0
+		for j, index in ipairs(rowInfo) do --计算最高的精灵
 			local height = heightArr[index]
-			if height > maxHeight then
-				maxHeight = height
+			if height > rowHeight then
+				rowHeight = height
 			end
 		end
-		local pointY = curY + maxHeight * 0.5
-		pointArrY[#pointArrY + 1] = pointY
-		curY = curY + maxHeight
+		local pointY = curY + rowHeight * 0.5 --当前行所有精灵的y坐标（正数，未取反）
+		rowHeightArr[#rowHeightArr + 1] = - pointY --从左往右，从上到下扩展，所以是负数
+		curY = curY + rowHeight --当前行的边缘坐标（正数）
+
+		if curY > maxHeight then
+			maxHeight = curY
+		end
 	end
 
-	-- dump(pointArrY)
+	self._maxWidth = maxWidth
+	self._maxHeight = maxHeight
 
-	--设置坐标
-	for i, sprite in ipairs(spriteArray) do
-		local pointX = pointArrX[i] --x坐标是直接读取
+	local pointArrY = {}
+
+	for i = 1, spriteNum do
 		local indexY = indexArrY[i] --y坐标是先读取精灵的行，然后再找出该行对应的坐标
-		local pointY = pointArrY[indexY]
-		sprite:setPosition(pointX, -pointY)
+		local pointY = rowHeightArr[indexY]
+		pointArrY[i] = pointY
 	end
 
+	return pointArrX, pointArrY
+end
+
+--调整位置
+function RichLabel:adjustPosition_()
+
+	local spriteArray = self._spriteArray
+
+	if not spriteArray then --还没创建
+		return
+	end
+
+	--获得每个精灵的宽度和高度
+	local widthArr, heightArr = self:getSizeOfSprites_(spriteArray)
+
+	--获得每个精灵的坐标
+	local pointArrX, pointArrY = self:getPointOfSprite_(widthArr, heightArr, self._dimensions)
+
+	for i, sprite in ipairs(spriteArray) do
+		sprite:setPosition(pointArrX[i], pointArrY[i])
+	end
 end
   
-function RichLabel:createSprite(parseArray)
+function RichLabel:createSprite_(parseArray)
 	local spriteArray = {}
 
 	for i, dic in ipairs(parseArray) do
@@ -169,14 +237,14 @@ function RichLabel:createSprite(parseArray)
 				local label = CCLabelTTF:create(word, fontName, fontSize)
 				label:setColor(fontColor)
 				spriteArray[#spriteArray + 1] = label
-				self:addChild(label)
+				self._containLayer:addChild(label)
 			end
 		elseif dic.image then
 			local sprite = CCSprite:create(dic.image)
 			local scale = dic.scale or 1
 			sprite:setScale(scale)
 			spriteArray[#spriteArray + 1] = sprite
-			self:addChild(sprite)
+			self._containLayer:addChild(sprite)
 		else
 			error("not define")
 		end
@@ -185,18 +253,18 @@ function RichLabel:createSprite(parseArray)
 	return spriteArray
 end
 
-function RichLabel:formatString(parseArray)
+function RichLabel:formatString_(parseArray)
 	for i,dic in ipairs(parseArray) do
 		local text = dic.text
 		if text then
-			local textArr = self:stringToChar(text)
+			local textArr = self:stringToChar_(text)
 			dic.textArray = textArr
 		end
 	end
 end
 
 --文字解析，按照顺序转换成数组，每个数组对应特定的标签
-function RichLabel:parseString(str)
+function RichLabel:parseString_(str)
 	local clumpheadTab = {} -- 标签头
 	--作用，取出所有格式为[xxxx]的标签头
 	for w in string.gfind(str, "%b[]") do 
@@ -214,9 +282,9 @@ function RichLabel:parseString(str)
 		string.gsub(ns, string.sub(ns, 2, #ns-1), function (w)
 			local n = string.find(w, "=")
 			if n then
-				local temTab = self:stringSplit(w, " ") -- 支持标签内嵌
+				local temTab = self:stringSplit_(w, " ") -- 支持标签内嵌
 				for k,pstr in pairs(temTab) do
-					local temtab1 = self:stringSplit(pstr, "=")
+					local temtab1 = self:stringSplit_(pstr, "=")
 					
 					local pname = temtab1[1]
 
@@ -234,7 +302,7 @@ function RichLabel:parseString(str)
 
 					local switchState = {
 						["fontColor"]	 = function()
-							tab["fontColor"] = self:convertColor(js)
+							tab["fontColor"] = self:convertColor_(js)
 						end,
 					} --switch end
 
@@ -279,7 +347,7 @@ end
 
 
 --[[解析16进制颜色rgb值]]
-function  RichLabel:convertColor(xStr)
+function  RichLabel:convertColor_(xStr)
     local function toTen(v)
         return tonumber("0x" .. v)
     end
@@ -295,7 +363,7 @@ function  RichLabel:convertColor(xStr)
 end
 
 -- string.split()
-function RichLabel:stringSplit(str, flag)
+function RichLabel:stringSplit_(str, flag)
 	local tab = {}
 	while true do
 		local n = string.find(str, flag)
@@ -312,7 +380,7 @@ function RichLabel:stringSplit(str, flag)
 end
 
 -- 拆分出单个字符
-function RichLabel:stringToChar(str)
+function RichLabel:stringToChar_(str)
     local list = {}
     local len = string.len(str)
     local i = 1 
